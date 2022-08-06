86b60c030a9bb6aff330362a83277f92710ce266
Алекс <dendyboy777@gmail.com>
2022-08-06 15:50:16 +0300

    задание 1-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+  // задание 1
+        int clientOS = 0;
+if (clientOS==1){
+    System.out.println("Установите версию приложения для iOS по ссылке");}
+else System.out.println("Установите версию приложения для Android по ссылке");
     }
+
+
 }
